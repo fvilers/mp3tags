0519250a2b9ec0783eef5a302ac0ef61b64ea7b3
Fabian Vilers <fabian.vilers@dev-one.com>
2020-11-30 12:07:37 +0100

    Display string ID3 tags

--- src/helpers.ts
+++ src/helpers.ts
@@ -2,3 +2,11 @@ import fs from "fs";
 import util from "util";
 
 export const fileExists = util.promisify(fs.exists);
+
+export function isString(value: any): value is string {
+  return typeof value === "string";
+}
+
+export function capitalize(value: string): string {
+  return value.charAt(0).toUpperCase() + value.slice(1);
+}
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import chalk from "chalk";
-import { fileExists } from "./helpers";
+import NodeID3 from "node-id3";
+import { capitalize, fileExists, isString } from "./helpers";
 
 export async function main(): Promise<void> {
   const [, , ...args] = process.argv;
@@ -13,5 +14,17 @@ export async function main(): Promise<void> {
   if (!(await fileExists(filename))) {
     console.error(chalk.red("File does not exists"));
     return;
+  }
+
+  const tags = NodeID3.read(filename) as Record<string, any>;
+  const keys = Reflect.ownKeys(tags).filter(isString);
+  const length = Math.max(...keys.map((key) => key.length));
+
+  for (const key of keys) {
+    const value = tags[key];
+
+    if (isString(value)) {
+      console.log(capitalize(key).padEnd(length, " "), chalk.green(tags[key]));
+    }
   }
 }
